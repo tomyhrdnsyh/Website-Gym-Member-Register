79ea22e46060b51ee59d852c2507a17418687930
Tomy <tomyhelda05@gmail.com>
2022-10-13 20:58:54 +0700

    benerin settings

--- gym/settings.py
+++ gym/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    # os.path.join(BASE_DIR, 'static'), 
     os.path.join(BASE_DIR, 'assets', 'css'), 
     os.path.join(BASE_DIR, 'assets', 'js'), 
     os.path.join(BASE_DIR, 'assets', 'fonts'), 
@@ -131,10 +131,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets', 'images', 'class'),
     os.path.join(BASE_DIR, 'assets', 'images', 'team'),
     ]
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
