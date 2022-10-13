80768a7499fcdbc84ec978d529e1448da4846c69
Tomy <tomyhelda05@gmail.com>
2022-10-13 21:23:32 +0700

    static files

--- gym/settings.py
+++ gym/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z@378$ku)jzo!#p_n&f9k-r8ul)139e84c21!wg5pxbnut2=08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets', 'css'), 
     os.path.join(BASE_DIR, 'assets', 'js'), 
