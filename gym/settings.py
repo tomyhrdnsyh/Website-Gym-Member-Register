bb8cf11850b8777ded960fd2e3fd04ae90d11c60
Tomy <tomyhelda05@gmail.com>
2022-10-13 22:06:16 +0700

    hasil setelah bisa ygy

--- gym/settings.py
+++ gym/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
-
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,7 +127,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets', 'images', 'class'),
     os.path.join(BASE_DIR, 'assets', 'images', 'team'),
     ]
-# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
