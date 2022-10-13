f37689f1fae8cc0ba56aaa42da34f6743860510c
Tomy <tomyhelda05@gmail.com>
2022-10-13 20:43:05 +0700

    the first time

--- gym/settings.py
+++ gym/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-z@378$ku)jzo!#p_n&f9k-r8ul)139e84c21!wg5pxbnut2=08
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
     os.path.join(BASE_DIR, 'assets', 'css'), 
     os.path.join(BASE_DIR, 'assets', 'js'), 
     os.path.join(BASE_DIR, 'assets', 'fonts'), 
@@ -127,10 +131,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets', 'images', 'class'),
     os.path.join(BASE_DIR, 'assets', 'images', 'team'),
     ]
-    
-    
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
