524a76b319bad66b01248856285be2a710508346
Tomy Hardiansyah <tomy@jkt1.ebdesk.com>
2022-10-25 20:10:32 +0700

    create login and register page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='homepage'),
     path('id/', views.dashboard_id, name='homepage'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
