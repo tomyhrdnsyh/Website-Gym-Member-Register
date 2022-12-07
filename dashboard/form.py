a596f6fdd17ab1e222af9deeacad320d02d1dd0f
Tomy <tomyhelda05@gmail.com>
2022-12-07 23:27:17 +0700

    finishing

--- dashboard/form.py
+++ dashboard/form.py
@@ -1,5 +1,3 @@
-from secrets import choice
-from urllib import request
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
